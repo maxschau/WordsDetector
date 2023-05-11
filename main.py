43700bdada7750e131cd6fbafd801de49073b0b0
Max Schau <max.torre.schau@gmail.com>
2023-05-11 14:07:08 +0200

    Added offset as args

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 from PyPDF2 import PdfReader
 
-offset_page_number = 18
 
 def load_spelling_differences(file_path):
     spelling_differences = {}
@@ -29,10 +28,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Find American spellings in a PDF.')
     parser.add_argument('-d', '--differences', required=True, help='Path to the spelling differences file.')
     parser.add_argument('-f', '--file', required=True, help='Path to the PDF file.')
+    parser.add_argument('-o', '--offset', required=False, help='Offset page number by this amount.', default=0)
     args = parser.parse_args()
 
     spelling_differences = load_spelling_differences(args.differences)
     found_words = read_pdf(args.file, spelling_differences)
+    offset = int(args.offset)
 
     for american, british, page in found_words:
-        print(f'Word "{american}" found on page {page+1 - offset_page_number} - consider change to "{british}"')
+        print(f'Word "{american}" found on page {page+1 - offset} - consider change to "{british}"')
